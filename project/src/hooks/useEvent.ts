d358fc6c8da79291df64fa8b8d80306745ef418e
Dmitrii <dp086@mail.ru>
2021-11-25 14:06:50 +0300

    clear comment code

--- project/src/hooks/useEvent.ts
+++ project/src/hooks/useEvent.ts
@@ -2,10 +2,7 @@ import { useEffect } from "react"
 
 export function useKeydownEvent(event: string, handler: (evt: KeyboardEvent) => void, passive = false) {
   useEffect(() => {
-    // initiate the event handler
     window.addEventListener('keydown', handler, passive)
-
-    // this will clean up the event every time the component is re-rendered
     return function cleanup() {
       window.removeEventListener('keydown', handler)
     }
